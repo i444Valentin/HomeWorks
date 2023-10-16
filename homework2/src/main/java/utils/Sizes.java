ea5b2eabd0216e306c8b68d8ccf423797ca2b15e
Walentin <valession@gmail.com>
2023-10-16 15:49:51 +0300

    Расширение класса Sizes. Добавление документации

--- homework2/src/main/java/utils/Sizes.java
+++ homework2/src/main/java/utils/Sizes.java
@@ -2,8 +2,24 @@ package utils;
 
 import packs.Pack;
 
+/**
+ * Утилитный класс, используемый
+ * для расчета размеров для возможного создания упаковок и контейнеров
+ */
 public final class Sizes {
 
+    //Запрещает создание объектов этого класса
+    private Sizes(){
+
+    }
+
+    /**
+     * Возвращает размер, вычисленный из количества
+     * упаковок и их размера
+     * @param pack - упаковка
+     * @param count - количество упаковок
+     * @return - новый размер
+     */
     public static Size fromItemCount(Pack pack, Integer count){
         Integer sizeH = pack.getSizeH();
         Integer sizeW = pack.getSizeW();
@@ -11,5 +27,22 @@ public final class Sizes {
         Integer totalSizeH = sizeH * count;
 
         return new Size(totalSizeH,sizeW,sizeD);
+    }
+
+    /**
+     * Вычисляет размеры, исходя из объема и высоты
+     * Подходит для вычисления размеров для емкостей для жидкостей (упаковок,
+     * бутылок и т.д.)
+     * Метод не учитывает особые формы упаковки (как в случае с бутылками),
+     * поэтому размеры могут быть неточными.
+     * @param volume - объем
+     * @param height - высота
+     * @return - вычисленные размеры
+     */
+    public static Size fromVolume(int volume,int height){
+        int square = volume / height;
+        int side = (int) Math.sqrt(square);
+
+        return new Size(side,height,side);
     }
 }
---
+++ homework2/src/main/java/utils/TypeForms.java
@@ -0,0 +1,8 @@
+package utils;
+
+/**
+ * Обозначает различные типы контейнеров
+ */
+public enum TypeForms {
+    FLAT_CONTAINER, HIGH2_CONTAINER,HIGH3_CONTAINER,FLAT_SQUARED_CONTAINER,CUBE,FLAT_RECTANGLE
+}
