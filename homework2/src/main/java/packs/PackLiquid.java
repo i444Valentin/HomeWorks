4ae6eb9b59fa3e9255834e4a8c757989728b97c9
Walentin <valession@gmail.com>
2023-10-16 15:51:38 +0300

    Добавление документации к классам Pack И PackLiquid

--- homework2/src/main/java/packs/PackLiquid.java
+++ homework2/src/main/java/packs/PackLiquid.java
@@ -6,6 +6,13 @@ import enums.Material;
 
 import java.util.ArrayList;
 
+/**
+ * Представляет класс упаковки с жидкостью
+ * Помимо базовых атрибутов, у класса есть атрибут объем
+ * Длины сторон указываются в сантиметрах (см)
+ * Объем высчитывается перемножением трех сторон и указывается в миллилитрах
+ *
+ */
 @Getter
 @Setter
 public class PackLiquid extends Pack {
@@ -13,13 +20,13 @@ public class PackLiquid extends Pack {
 
     public PackLiquid(Integer sizeW, Integer sizeH, Integer sizeD, Material material) {
         super(sizeW, sizeH, sizeD, material);
-        volume = sizeW*sizeH*sizeD*1000;
+        volume = sizeW*sizeH*sizeD;
         content = new ArrayList<Integer>(volume);
     }
 
     public PackLiquid(Integer sizeW, Integer sizeH, Integer sizeD, String mfr, Material material) {
         super(sizeW, sizeH, sizeD, mfr, material);
-        volume = sizeW*sizeH*sizeD*1000;
+        volume = sizeW*sizeH*sizeD;
         content = new ArrayList<Integer>(volume);
     }
 }
